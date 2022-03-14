8a1471c3ac66b4a2a64c686a212c220ca63b616a
Brandon Wang <thebrandonwang@gmail.com>
2022-03-14 15:40:01 +0800

    second edition

--- c_to_f.py
+++ c_to_f.py
@@ -1,4 +1,5 @@
 celsius = input('what is the temperture?')
 celsius = float(celsius)
 fahrenheit = celsius / 5 * 9 + 32
-print('fahrenheit is', fahrenheit) 
+print('fahrenheit is', fahrenheit)
+print('second edit')
